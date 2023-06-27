f8a4fed65dc2bb1685a84425aa68986f3039f960
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-06-27 19:08:52 +0530

    chore: Refactor authentication and pages components (#9)

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,1 +1,4 @@
 export { default as SignInForm } from "./SignInForm";
+export { default as SignUpForm } from "./SignUpForm";
+export { default as UpdatePasswordForm } from "./UpdatePasswordForm";
+export { default as UpdateUserForm } from "./UpdateUserForm";
